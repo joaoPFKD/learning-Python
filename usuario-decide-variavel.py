35b07de72dc2cbda441b0e291207c9f5f689ddb8
Joao Paulo Franco <joaopaulo.kruppell@gmail.com>
2025-07-28 22:20:30 -0300

    Create usuario-decide-variavel.py

---
+++ usuario-decide-variavel.py
@@ -0,0 +1,32 @@
+x = y = adicao = subtracao = multiplicacao = divisaoreal = divisaointeira = modulo = potencia = 0
+
+x = int(input('digite o valor para a soma: '))
+y = int(input('digite outro valor para a soma: '))
+
+print( x, ' e ', y)
+
+adicao = x + y 
+subtracao = x - y
+multiplicacao = x * y
+divisaoreal = x / y
+divisaointeira = x // y
+modulo = x % y
+potencia = x ** y
+
+
+print ('a soma dos valores e igual= ' ,adicao )
+print ('')
+print ('a subtracao dos valores e igual= ' ,subtracao )
+print ('')
+print ('a multiplicacao dos valores e igual= ' ,multiplicacao )
+print ('')
+print ('a divisao real dos valores e igual= ' ,divisaoreal )
+print ('')
+print ('a divisao inteira  dos valores e igual= ' ,divisaointeira )
+print ('')
+print ('a modulo dos valores e igual= ' ,modulo )
+print ('')
+print ('a potencia dos valores e igual= ' ,potencia )
+
+
+
